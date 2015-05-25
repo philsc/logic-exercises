a5c59cff224afc547ef74be726e9ca889f0f8c37
Philipp Schrader <philipp.schrader@gmail.com>
2015-05-25 15:24:15 -0700

    Add testbench and attempt to fix compilation errors.

--- bitstream-101-sequence/logic_101.v
+++ bitstream-101-sequence/logic_101.v
@@ -18,39 +18,43 @@ reg [1:0] state;
 // Functionality
 always @ (posedge clock)
 begin
-  if (reset == 1'b1) begin
+  if (reset == 1'b0) begin
     oOUT <= 1'b0;
     state <= 2'b00;
   end
   else begin
     case (state)
-      2'b00:
+      2'b00: begin
         if (iIN == 1'b1) begin
           state <= 2'b01;
         end
         oOUT <= 1'b0;
+      end
 
-      2'b01:
+      2'b01: begin
         if (iIN == 1'b0) begin
           state <= 2'b10;
         end
         oOUT <= 1'b0;
+      end
 
-      2'b10:
+      2'b10: begin
         if (iIN == 1'b0) begin
           state <= 2'b00;
         end else begin
           state <= 2'b11;
         end
         oOUT <= 1'b0;
+      end
 
-      2'b11:
+      2'b11: begin
         if (iIN == 1'b0) begin
           state <= 2'b00;
         end else begin
           state <= 2'b01;
         end
         oOUT <= 1'b1;
+      end
     endcase
   end
 end
---
+++ bitstream-101-sequence/logic_101_tb.v
@@ -0,0 +1,35 @@
+`include "logic_101.v"
+
+module logic_101_tb();
+
+reg oCLK;
+reg oRST;
+reg oIN;
+
+wire iOUT;
+
+initial begin
+  oRST = 1;
+  oCLK = 0;
+  oIN = 0;
+
+  $display ("time\t clk reset input output");
+  $monitor ("%g\t %b   %b     %b      %b",
+      $time, oCLK, oRST, oIN, iOUT);
+
+  #5 oRST = 0;  // Assert the reset.
+  #5 oRST = 1;  // De-assert the reset.
+  #20 oIN = 1;
+  #5 oIN = 0;
+  #5 oIN = 1;
+  #100 $finish
+end
+
+// Toggle clock every 5 ticks.
+always begin
+  #5 oCLK = ~oCLK;
+end
+
+logic_101(oCLK, oRST, oIN, iOUT);
+
+endmodule
